4c783931fc37fe0d2f84f0da44d8d1612a64534e
srinjay-yactraq <srinjay@yactraq.com>
2025-08-26 01:37:57 +0530

    api to get all repo for dashboard and clear api added

--- src/controllers/repoController.js
+++ src/controllers/repoController.js
@@ -10,7 +10,7 @@ exports.searchAndStoreRepos = async (req, res) => {
     if(keywordRelatedData.length){
       res.status(200).json({ message: 'Data in DB.' });
     }else{
-      const response = await axios.get(`https://api.github.com/search/repositories?q=${keyword}&per_page=20`);
+      const response = await axios.get(`https://api.github.com/search/repositories?q=${keyword}`);
       const repos = response.data.items;
   
       for (const repo of repos) {
@@ -37,16 +37,44 @@ exports.searchAndStoreRepos = async (req, res) => {
 };
 
 exports.getReposPaginated = async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
+  const page = parseInt(req.query.page);
+  const keyword = req.query.keyword;
+  
   const limit = 10;
   try {
-    const repos = await Repo.find()
+    if(keyword == "All"){
+      const repos = await Repo.find()
       .skip((page - 1) * limit)
       .limit(limit)
       .sort({ stargazers_count: -1 });
-    const count = await Repo.countDocuments();
-    res.json({ repos, totalPages: Math.ceil(count / limit), currentPage: page });
+      const count = await Repo.countDocuments();
+      res.json({ repos, totalPages: Math.ceil(count / limit), currentPage: page });
+    } else {
+      const repos = await Repo.find({ keyword: keyword })
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .sort({ stargazers_count: -1 });
+      const count = await Repo.countDocuments({ keyword: keyword });
+      res.json({ repos, totalPages: Math.ceil(count / limit), currentPage: page });
+    }
   } catch (err) {
     res.status(500).json({ error: 'Failed to get repos' });
+  }
+};
+
+exports.clearRepos = async (req, res) => {
+ 
+  try {
+   
+    const deleted = await Repo.deleteMany({})
+
+    if (deleted.deletedCount > 0) {
+      res.status(200).json({ message: 'All repos deleted successfully.' });
+    } else {
+      res.status(200).json({ message: 'No repos found to delete.' });
+    }
+
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete repos' });
   }
 };
--- src/routes/repoRoutes.js
+++ src/routes/repoRoutes.js
@@ -4,5 +4,6 @@ const repoController = require('../controllers/repoController');
 
 router.post('/search', repoController.searchAndStoreRepos);
 router.get('/repos', repoController.getReposPaginated);
+router.delete('/clear-repos', repoController.clearRepos);
 
 module.exports = router;
